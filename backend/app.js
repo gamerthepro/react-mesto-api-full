c819c114514ac19920b3779ac17f090a7c7c31c8
Artem Polonnikov <gamer.thepro@yandex.ru>
2021-07-26 19:06:02 +0300

    delet

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const ; = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
