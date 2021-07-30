536a53724d5d78aee9e575184acfa8fc5558c540
Artem Polonnikov <gamer.thepro@yandex.ru>
2021-07-30 16:13:50 +0300

    fix app.js

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,4 @@
-const ; = require('express');
+const = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
